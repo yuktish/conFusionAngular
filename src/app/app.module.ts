cbaf3383ddb0d626f9cc2a8cc4000b512d3f8a33
yuktish <syukti976@gmail.com>
2019-06-12 13:55:37 +0530

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,8 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { baseURL } from './shared/baseurl';
-import { ProcessHTTPMsgService } from './services/process-httpmsg.service'
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+import { HighlightDirective } from './directives/highlight.directive'
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service'
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
